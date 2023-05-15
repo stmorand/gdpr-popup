908b1e2ec4b410366a01604172cd3597ba93f39e
Stephan <stephan@morand.pro>
2023-05-15 17:15:14 +0200

    style adapted and removal of a tools folder > every tools are described in tools.json

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+let tools;
 function getCookie(name){
     if(document.cookie.length === 0)
         return null;
@@ -36,7 +37,6 @@ function toggleConsent(consentType) {
         console.log("giveConsent");
         giveConsent(consentType)
     }
-    alert(getAgreedTools());
 }
 
 /*  give consent for an app type */
@@ -56,8 +56,68 @@ function removeConsent(consentType) {
     }
 }
 
+function showCookiePopup() {
+    document.getElementById("cookie-popup").style.display = 'block';
+}
+
 function initCheckboxes() {
     for (let i = 0; i < getAgreedTools().length; i++) {
         document.getElementById('chk-'+getAgreedTools()[i]).checked = true;
     }
+}
+
+function confirmConsent() {
+    document.cookie = 'consentConfirmed=' + Date.now();
+    setupTools();
+}
+function isConsentGiven() {
+    return getCookie('consentConfirmed')!=null
+}
+
+/* retrieve, either by type, or all  */
+/* can be "technical", "third-party","ads","optional" or "all" */
+function getTools(toolType = "all") {
+    console.log(tools);
+}
+
+function loadToolsJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'tools.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState === 4 && xobj.status === 200) {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadToolsJSON(function(response) {
+        // Parse JSON string into object
+        tools = JSON.parse(response);
+        if (Object.keys(tools).length > 0) {
+            if (
+                (tools["technical"] === null || tools["technical"].length === 0)&&
+                (tools["third-party"] === null || tools["third-party"].length === 0)&&
+                (tools["ads"] === null || tools["ads"].length === 0)&&
+                (tools["optional"] === null || tools["optional"].length === 0)
+                ) {
+                document.getElementById("cookie-popup").style.display = "none";
+            }
+            else {
+                document.getElementById("chk-technical-container").style.display = (tools["technical"] !== null && tools["technical"].length > 0) ? "block" :"none";
+                document.getElementById("chk-third-party-container").style.display = (tools["third-party"] !== null && tools["third-party"].length > 0) ? "block" :"none";
+                document.getElementById("chk-ads-container").style.display = (tools["ads"] !== null && tools["ads"].length > 0) ? "block" :"none";
+                document.getElementById("chk-optional-container").style.display = (tools["optional"] !== null && tools["optional"].length > 0) ? "block" :"none";
+            }
+        }
+        getTools();
+    });
+}
+
+/* When the consent is given the tools provided in tools folder are setup*/
+function setupTools() {
+    console.log("setting up the tools");
 }
